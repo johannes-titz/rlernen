ef6796daa6f48c9b835f518637530bdff230cf8c
Johannes Titz <johannes@titz.science>
2025-06-29 09:27:18 +0200

    add homework 7

---
+++ inst/tutorials/tag7/homework7.R
@@ -0,0 +1,47 @@
+# Hausaufgabe 7
+#
+# Wir verwenden den Datensatz 'mtcars', der in R verfügbar ist. Es werden nur
+# ausgewählte metrische Variablen untersucht: mpg, disp, hp, wt, qsec
+#
+# Lade den Datensatz und selektiere zunächst diese Variablen:
+
+
+# Führe nun eine Faktoranalyse mit einem Faktor durch. Nutze die
+# Varimax-Rotation.
+
+
+# Wie groß ist die Varianzaufklärung des Faktors?
+# Wie groß ist die Faktorladung für die Variable 'wt'?
+# Wie groß ist die Kommunalität dieser Variable?
+
+
+# Führe nun eine Faktoranalyse mit zwei Faktoren durch.
+# Welche Items laden jeweils auf welchen Faktoren?
+# Erscheint die Zweifaktorlösung sinnvoller als die Einfaktorlösung?
+
+
+# Teil 2 -----------------------------------------------------------------------
+#
+# Wir nutzen den Datensatz 'USArrests', der direkt in R verfügbar ist. Der
+# Datensatz enthält Verbrechen pro 100 000 Einwohner in den 50 US Bundesstaaten
+# aus dem Jahr 1973.
+
+# Führe eine Cluster-Analyse mit hclust durch. Standardisiere zunächst die
+# Variablen.
+
+
+# Erstelle ein Dendrogramm.
+
+
+# Wie viele Cluster erscheinen im Dendrogramm sinnvoll? Begründe kurz.
+
+
+# Schneide den Baum so, dass 4 Cluster entstehen. Wie groß sind die jeweiligen
+# Cluster?
+
+
+# Welche Bundesstaaten gehören jeweils zu Cluster 1?
+
+
+# Erstelle ein Streudiagramm für die zwei Variablen Assault und UrbanPop und
+# färbe die Punkte nach Clusterzugehörigkeit.
