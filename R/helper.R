0528f31b56b93a9e9ebc8dd1390e6f51aaba47b5
Johannes Titz <johannes@titz.science>
2026-03-30 11:19:50 +0200

    update calendar

--- R/helper.R
+++ R/helper.R
@@ -5,24 +5,25 @@
 calendar <- function() {
   kurs_kalender <- "
 Datum,Thema
-07.04.2025,Organisatorisches
-14.04.2025,Einf\u00fchrung
-21.04.2025,Kein Kurs - OSTERMONTAG
-28.04.2025,Daten-Gerangel
-05.05.2025,Deskriptive Statistik
-12.05.2025,t-Test und ANOVA
-19.05.2025,Kontrastanalyse
-26.05.2025,Non-parametrische Tests
-02.06.2025,Regression
-09.06.2025,Kein Kurs - PFINGSTMONTAG
-16.06.2025,Faktorenanalyse
-23.06.2025,Cluster-Analyse
-30.06.2025,Meta-Analyse
-07.07.2025,Freies Thema / Vertiefung
-14.07.2025,Pr\u00fung
+08.04.2026,Organisatorisches
+15.04.2026,Einführung
+22.04.2026,Daten-Gerangel
+29.04.2026,Deskriptive Statistik
+06.05.2026,t-Test und ANOVA
+13.05.2026,Kontrastanalyse
+20.05.2026,Non-parametrische Tests
+27.05.2026,Regression
+03.06.2026,Faktorenanalyse
+10.06.2026,Cluster-Analyse
+17.06.2026,Kein Kurs - Campus- und Sportfest
+24.06.2026,Meta-Analyse
+01.07.2026,Freies Thema / Vertiefung
+08.07.2026,Zusatztermin / Übung
+15.07.2026,Prüfung
 "
 
   df <- read.csv(textConnection(kurs_kalender), stringsAsFactors = FALSE)
+  df$Datum <- as.Date(df$Datum, format = "%d.%m.%Y")
   df
 }
 
@@ -31,8 +32,8 @@ calendar_ical <- function() {
   df <- calendar()
   names(df) <- c("Start Date", "Subject")
   df$`End Date` <- df$`Start Date`
-  df$`Start Time` <- "5:30pm"
-  df$`End Time` <- "7:00pm"
+  df$`Start Time` <- "1:30pm"
+  df$`End Time` <- "3:00pm"
   df$Location <- "4/046"
   df$UID <- ids::adjective_animal(nrow(df))
   df
