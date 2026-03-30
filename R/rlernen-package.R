302e5cec320bac077493a8fbfd57ae26962cbef3
Johannes Titz <johannes@titz.science>
2026-03-30 16:08:02 +0200

    docs: package.R for imports

---
+++ R/rlernen-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom gradethis fail
+#' @importFrom gradethis pass
+#' @importFrom knitr knit_print
+#' @importFrom learnr question_numeric
+#' @importFrom learnr tutorial_options
+## usethis namespace: end
+NULL
